58a3f3011e2625f62f3aaf5f062d968f206c6805
meryemyildiz <meryemyldzz.75@gmail.com>
2022-03-12 01:27:53 +0300

    MvcProje

---
+++ MvcStok/MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,60 @@
+﻿using MvcStok.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        Mvc_Db_StokEntities db = new Mvc_Db_StokEntities();
+        public ActionResult Index(int sayfa=1)
+        {
+            var degerler = db.TBLKATEGORILER.ToList().ToPagedList(sayfa, 4);
+           // var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniKategori");
+            }
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult SIL (int id)
+        {
+            var kategori = db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult GUNCELLE (int id)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("GUNCELLE",ktgr);
+        }
+        public ActionResult MGuncelle(TBLKATEGORILER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD = p1.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
